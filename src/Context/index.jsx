971a081555bd2c1774f01438fa46e7f7614ce5f5
josue <josuepolania@users.noreply.github.com>
2024-02-15 21:58:26 -0500

    mostrando productos en ProductDetail

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -3,11 +3,18 @@ import { createContext, useState } from "react"
 export const ShoppingCartContext = createContext()
 
 export const ShoppingCartProvaider = ({children}) => {
-
+     //shopping cart - increment quantify
     const [count, setCount] = useState(0)
+
+
+    //product Detail - open/clese
     const [isProductDetailOpen, setIsProductDetailOpen] = useState(false)
     const openProductDetail = () => setIsProductDetailOpen(true)
     const closeProductDetail = () => setIsProductDetailOpen(false)
+
+
+    //product Detail - show product
+    const [productToShow, setProductToShow ] = useState({})
 
      return (
         <ShoppingCartContext.Provider value={{
@@ -15,7 +22,9 @@ export const ShoppingCartProvaider = ({children}) => {
             setCount,
             openProductDetail,
             closeProductDetail,
-            isProductDetailOpen
+            isProductDetailOpen,
+            productToShow,
+            setProductToShow
         }}>
             {children}
         </ShoppingCartContext.Provider>
--- src/componentes/ProductDetail/index.jsx
+++ src/componentes/ProductDetail/index.jsx
@@ -7,15 +7,30 @@ import "./styles.css"
 
 const ProductDetail = () => {
     const Context = useContext(ShoppingCartContext)
+
+
     return (
         <aside 
-            className={`${Context.isProductDetailOpen ? "flex" : "hidden"} backdrop:product-detail flex-col fixed right-0 border border-black rounded-r-lg bg-white`}>
+            className={`${Context.isProductDetailOpen ? "flex" : "hidden"} product-detail flex-col fixed right-0 border border-black rounded-lg bg-white`}>
             <div className="flex justify-between items-center p-6">
                 <h2 className="font-medium text-xl">Detail</h2>
                 <div>
-                    <XMarkIcon className="h-6 w-6 text-black -500"></XMarkIcon>
+                    <XMarkIcon 
+                    className="h-6 w-6 text-black cursor-pointer"
+                    onClick={() => Context.closeProductDetail()}></XMarkIcon>
                 </div>
             </div>
+            <figure className="px-6">
+                <img 
+                className="w-full h-full rounded-lg" 
+                src={Context.productToShow.images} 
+                alt={Context.productToShow.title} />
+            </figure>
+            <p className="flex flex-col p-6">
+                <span className="font-medium text-2xl">${Context.productToShow.price}</span>
+                <span className="font-medium text-md">${Context.productToShow.title}</span>
+                <span className="font-light text-sm">${Context.productToShow.description}</span>
+            </p>
         </aside>
     )
 }
